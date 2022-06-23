371ba4a33e368dc6bb9f56ae1daef2c89c9aa2ce
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-23 23:38:13 +0300

    Add project files.

---
+++ Csharp7/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int toplam = 0;
+
+Console.WriteLine("İlk sayıyı giriniz");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("İkinci sayıyı giriniz");
+int b = Convert.ToInt32(Console.ReadLine());
+
+for (int i = a+1; i < b; i++)
+{
+    toplam = toplam + i;
+}
+Console.WriteLine($"İki sayı arasındaki sayıların toplamı = {toplam} ");
+
+
+
